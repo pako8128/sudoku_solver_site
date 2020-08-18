0210de7e031c46b2f778d248e1b48ca02251761d
PaKo <pako8128@protonmail.com>
2020-08-19 00:05:34 +0200

    succesfully making API requests and displaying the result

---
+++ js/client.js
@@ -0,0 +1,50 @@
+const API_ADDRESS = 'https://sudokus-solver.herokuapp.com/api/solve';
+
+const inputs = document.querySelectorAll('input');
+const solveButton = document.querySelector('#solveButton');
+
+console.log(inputs);
+
+function readSudoku() {
+	const sudoku = [];
+	for (var y = 0; y < 9; y++) {
+		const row = [];
+
+		for (var x = 0; x < 9; x++) {
+			const num = inputs.item(y * 9 + x).value;
+			if (num == "") {
+				row[x] = 0;
+			} else {
+				row[x] = Number(num);
+			}
+		}
+
+		sudoku[y] = row;
+	}
+
+	return sudoku;
+}
+
+function writeSudoku(sudoku) {
+	for (var y = 0; y < 9; y++) {
+		for (var x = 0; x < 9; x++) {
+			inputs.item(y * 9 + x).value = sudoku[y][x];
+		}
+	}
+}
+
+function solveSudoku() {
+	const sudoku = readSudoku();
+
+	fetch(API_ADDRESS, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(sudoku),
+	})
+	.then(resp => resp.json())
+	.then(data => writeSudoku(data));
+}
+
+solveButton.addEventListener('click', solveSudoku);
